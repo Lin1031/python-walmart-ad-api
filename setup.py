67f074cc99cc82cde68bd5397931077042c0941c
coderjiang <coderjiang@yeah.net>
2023-08-23 10:23:20 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     install_requires=[
         "requests",
     ],
-    packages=['ad_api', 'ad_api.api', 'ad_api.auth', 'ad_api.base']
+    packages=['ad_api', 'ad_api.api', 'ad_api.auth', 'ad_api.base'],
     url='https://github.com/Lin1031/python-walmart-ad-api.git',
     license='MIT',
     author='Lin1031',
